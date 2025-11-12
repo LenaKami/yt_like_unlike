deb1600c3eff08859dc0a28a21da39f1660a5fe6
LenaKami <lenakami07@gmail.com>
2025-11-12 13:25:39 +0100

    Register + login

--- player/src/userdb/RegistrationForm.tsx
+++ player/src/userdb/RegistrationForm.tsx
@@ -52,7 +52,7 @@ export const RegistrationForm = () => {
         <div className="flex flex-col items-center justify-center px-12 py-8 mx-auto md:h-screen lg:py-0">
           <a href="#" className="flex items-center mb-6 text-2xl font-semibold text-white">
             <img className="w-8 h-8 mr-2" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/logo.svg" alt="logo" />
-            PlayerYT
+            StudyBeats
           </a>
           <div className="w-full rounded-lg shadow md:mt-0 sm:max-w-md xl:p-0 login-box">
             <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
